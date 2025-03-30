0a828c7e28ab93181b912f6db4fc0df2016121d2
D2moon <1970353515@qq.com>
2025-03-30 22:44:04 +0800

    🌟完成了VAE部分

---
+++ sd/attention.py
@@ -0,0 +1,59 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import math
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, casual_mask: False)-> torch.Tensor:
+        # x: (BatchSize, SeqLen, Dim)
+
+        input_shape = x.shape
+        batch_size, sequence_length, d_embed = input_shape
+
+        intermin_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+
+        # (BatchSize, SeqLen, Dim) -> (BatchSize, SeqLen, 3*Dim) -> 3 tensors of (BatchSize, SeqLen, Dim)
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+
+        # (BatchSize, SeqLen, Dim) -> (BatchSize, SeqLen, H, Dim/H) -> (BatchSize, H, SeqLen, Dim/H)
+        q = q.view(intermin_shape).transpose(1, 2)
+        k = k.view(intermin_shape).transpose(1, 2)
+        v = v.view(intermin_shape).transpose(1, 2)
+
+        # weight: (BtchSize, H, SeqLen, SeqLen)
+        weight = q @ k.transpose(-2, -1)
+
+        if casual_mask:
+            # mask where the upper triangle is 1 and lower triangle is 0
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill_(mask, -torch.inf)
+
+        weight /= math.sqrt(self.d_head)
+
+        weight = F.softmax(weight, dim=-1)
+
+        # output: (BatchSize, H, SeqLen, Dim/H)
+        output = weight @ v
+
+        # (BatchSize, H, SeqLen, Dim/H) -> (BatchSize, SeqLen, H, Dim/H))
+        output = output.transpose(1, 2)
+
+        output = output.reshape(input_shape)
+
+        # (BatchSize, SeqLen, Dim) -> (BatchSize, SeqLen, Dim)
+        output = self.out_proj(output)
+
+        # (BatchSize, SeqLen, Dim)
+        return output
+
+
+
+
--- sd/decoder.py
+++ sd/decoder.py
@@ -12,5 +12,125 @@ class VAE_ResidualBlock(nn.Module):
         self.groupnorm_2 = nn.GroupNorm(32, out_channels)
         self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:   
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (BatchSize, in_channels, Height, Width)
+
+        residue = x
+
+        x = self.groupnorm_1(x)
+
+        s = F.silu(x)
+
+        x = self.conv_1(s)
+
+        x = self.groupnorm_2(x)
+
+        x = F.silu(x)
+
+        x = self.conv_2(x)
+
+        return x + self.residual_layer(residue)
+
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels:int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (BatchSize, channels, Height, Width)
+
+        residue = x
+
+        n, c, h, w = x.shape
+
+        # (BatchSize, channels, Height, Width) -> (BatchSize, channels, Height*Width)
+        x = x.view(n, c, h*w)
+
+        # (BatchSize, channels, Height*Width) -> (BatchSize, Height*Width, channels)
+        x = x.transpose(-1, -2)
+
+        # (BatchSize, Height*Width, channels) -> (BatchSize, Height*Width, channels)
+        x = self.attention(x)
+
+        # (BatchSize, Height*Width, channels) -> (BatchSize, channels, Height*Width)
+        x = x.transpose(-1, -2)
+        
+        # (BatchSize, channels, Height*Width) -> (BatchSize, channels, Height, Width)
+        x = x.view((n, c, h, w))
+
+        x += residue
+
+        return x
+
 
 class VAE_Decoder(nn.Sequential):
+
+    def __init__(self):
+        super.__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (BatchSize, 512, Height/8, Width/8) -> (BatchSize, 512, Height/8, Width/8)
+            VAE_ResidualBlock(512, 512),
+
+            # (BatchSize, 512, Height/8, Width/8) -> (BatchSize, 512, Height/4, Width/4)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (BatchSize, 512, Height/4, Width/4) -> (BatchSize, 512, Height/2, Width/2)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+
+            # (BatchSize, 256, Height/4, Width/4) -> (BatchSize, 256, Height, Width)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+
+            nn.GroupNorm(32, 128),
+
+            nn.SiLu(),
+
+            # (BatchSize, 128, Height, Width) -> (BatchSize, 3, Height, Width)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (BatchSize, 4, Height/8, Width/8)
+
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        # x: (BatchSize, 3, Height, Width)
+        return x
