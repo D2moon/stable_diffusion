4c5487e8ab9a353d815b287ba2bf3b6ef5ff8b2a
D2moon <1970353515@qq.com>
2025-04-13 15:31:00 +0800

    🌟增加clip模块

---
+++ sd/clip.py
@@ -0,0 +1,90 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from attention import SelfAttention
+
+
+class CLIPEmbedding(nn.Module):
+
+    def __init__(self, n_vocab: int, n_embd: int, n_tokens: int):
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        self.positon_embedding = nn.Parameter(torch.zeros(n_tokens, n_embd))
+
+    def forward(self, tokens):
+        # (BatchSize, SeqLen) -> (BatchSize, SeqLen, Dim) Dim = 768
+        x = self.token_embedding(tokens)
+
+        x += self.positon_embedding
+
+        # (BatchSize, SeqLen, Dim)
+        return x
+
+
+class CLIPLayer(nn.Module):
+
+    def __init__(self, n_head: int, n_embd: int):
+        super().__init__()
+
+        self.layernorm_1 = nn.LayerNorm(n_embd)
+        self.attention = SelfAttention(n_head, n_embd)
+        self.layernorm_2 = nn.LayerNorm(n_embd)
+        self.linear_1 = nn.Linear(n_embd, n_embd * 4)
+        self.linear_2 = nn.Linear(n_embd * 4, n_embd)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (BatchSize, SeqLen, Dim)
+
+        residue = x
+
+        # SELF ATTENTION
+
+        x = self.layernorm_1(x)
+        
+        x = self.attention(x, casual_mask=True)
+
+        x += residue
+
+        # FEEDFORWARD LAYER
+
+        residue = x
+
+        x = self.layernorm_2(x)
+
+        x = self.linear_1(x)
+
+        x = x * torch.sigmoid(1.702 * x) # QuickGELU activation function
+
+        x = self.linear_2(x)
+
+        x += residue
+
+        return x
+
+
+class CLIP(nn.Module):
+
+    def __init__(self):
+        self.embedding = CLIPEmbedding(49408, 768, 77)
+
+        self.layers = nn.Module([
+            CLIPLayer(12, 768) for i in range(12)
+        ])
+
+        self.layernorm = nn.LayerNorm(768)
+
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        tokens = tokens.type(torch.long)
+
+        # (BatchSize, SeqLen) -> (BatchSize, SeqLen, Dim) Dim = 768
+        state = self.embedding(tokens)
+
+        for layer in self.layers:
+            state = layer(state)
+
+        # (BatchSize, SeqLen, Dim)
+        output = self.layernorm(state)
+
+        return output
+        
--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-import transformers
-import lightning
-import PIL
-import tqdm
-
-print("Torch Version:", torch.__version__)
-print("CUDA Available:", torch.cuda.is_available())
